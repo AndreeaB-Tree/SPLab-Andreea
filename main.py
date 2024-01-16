43af346081f16103d86989df7fea786000ee88d3
Brad Andreea <andreeaalexandra861@gmail.com>
2024-01-16 07:06:33 +0200

    lab6 changes

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from classes.Section import Section
-from classes.Paragraph import Paragraph
-from classes.AlignCenter import AlignCenter
-from classes.AlignLeft import AlignLeft
-from classes.AlignRight import AlignRight
+from spring import TransientComponent, SingletonComponent, ClientComponent
+
+transient_component = TransientComponent()
+transient_component.operation()
+
+singleton_component = SingletonComponent()
+singleton_component.operation()
 
 
-cap1 = Section("Capitolul 1")
-p1 = Paragraph("Paragraf 1")
-p2 = Paragraph("Paragraf 2")
-p3 = Paragraph("Paragraf 3")
-p4 = Paragraph("Paragraf 4")
-
-cap1.add(p1)
-cap1.add(p2)
-cap1.add(p3)
-cap1.add(p4)
-
-cap1.printElement()
-
-
-p1.setAlignStrategy(AlignCenter())
-p2.setAlignStrategy(AlignLeft())
-p3.setAlignStrategy(AlignRight())
-
-
-cap1.printElement()
+client = ClientComponent(transient_component, singleton_component)
+client.operation()
